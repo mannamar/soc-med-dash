ef6185323c447f06b1de74f662302c93861f7b19
Amardeep Mann <amar2slick@gmail.com>
2023-03-17 09:59:19 -0700

    Added Top BG Div

--- src/components/Header.js
+++ src/components/Header.js
@@ -17,18 +17,21 @@ export default function Header() {
     }
 
     return (
-        <Container className="headCont d-flex justify-content-between">
+        <>
+            <div className='bg-box'></div>
+            <Container className="headCont d-flex justify-content-between">
 
-            <div>
-                <p className="title">Social Media Dashboard</p>
-                <p className="subTitle">Total Followers: 23,004</p>
-            </div>
+                <div>
+                    <p className="title">Social Media Dashboard</p>
+                    <p className="subTitle">Total Followers: 23,004</p>
+                </div>
 
-            <div className="switchCont d-flex">
-                <p className="switchText">Dark Mode</p>
-                <Form.Switch onChange={toggleTheme}/>
-            </div>
-            
-        </Container>
+                <div className="switchCont d-flex">
+                    <p className="switchText">Dark Mode</p>
+                    <Form.Switch onChange={toggleTheme}/>
+                </div>
+                
+            </Container> 
+        </>
     )
 }
