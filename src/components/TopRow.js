3a1f6b6da1f08cefe85d1c1f131f72e8ec0970f8
Amardeep Mann <amar2slick@gmail.com>
2023-03-17 08:40:33 -0700

    Fixed top border

--- src/components/TopRow.js
+++ src/components/TopRow.js
@@ -13,16 +13,16 @@ export default function TopRow() {
         <Container className="topRowCont">
             <Row className="gx-4">
                 <Col>
-                    <BigCard icon={fbIcon} user='@nathanf' num='1987' type='FOLLOWERS' arrow={upIcon} textColor='green-text' numChange='12' border='blue-border'/>
+                    <BigCard icon={fbIcon} user='@nathanf' num='1987' type='FOLLOWERS' arrow={upIcon} textColor='green-text' numChange='12' border='fb-border'/>
                 </Col>
                 <Col>
-                    <BigCard icon={twIcon} user='@nathanf' num='1044' type='FOLLOWERS' arrow={upIcon} textColor='green-text' numChange='99' border='blue-border'/>
+                    <BigCard icon={twIcon} user='@nathanf' num='1044' type='FOLLOWERS' arrow={upIcon} textColor='green-text' numChange='99' border='tw-border'/>
                 </Col>
                 <Col>
-                    <BigCard icon={igIcon} user='@realnathanf' num='11k' type='FOLLOWERS' arrow={upIcon} textColor='green-text' numChange='99' border='blue-border'/>
+                    <BigCard icon={igIcon} user='@realnathanf' num='11k' type='FOLLOWERS' arrow={upIcon} textColor='green-text' numChange='99' border='ig-border'/>
                 </Col>
                 <Col>
-                    <BigCard icon={ytIcon} user='Nathan F.' num='1044' type='SUBSCRIBERS' arrow={downIcon} textColor='red-text' numChange='99' border='blue-border'/>
+                    <BigCard icon={ytIcon} user='Nathan F.' num='1044' type='SUBSCRIBERS' arrow={downIcon} textColor='red-text' numChange='99' border='yt-border'/>
                 </Col>
             </Row>
         </Container>
