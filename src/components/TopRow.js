d4555c9861bd594116bcf3da64dcab590ab89760
Amardeep Mann <amar2slick@gmail.com>
2023-10-02 00:30:26 -0700

    Updated usernames

--- src/components/TopRow.js
+++ src/components/TopRow.js
@@ -13,16 +13,16 @@ export default function TopRow() {
         <Container className="topRowCont">
             <Row className="gx-4 gy-4">
                 <Col xs={12} md={6} lg={3}>
-                    <BigCard icon={fbIcon} user='@nathanf' num='1987' type='FOLLOWERS' arrow={upIcon} textColor='green-text' numChange='12' border='fb-border'/>
+                    <BigCard icon={fbIcon} user='@mannamar' num='1987' type='FOLLOWERS' arrow={upIcon} textColor='green-text' numChange='12' border='fb-border'/>
                 </Col>
                 <Col xs={12} md={6} lg={3}>
-                    <BigCard icon={twIcon} user='@nathanf' num='1044' type='FOLLOWERS' arrow={upIcon} textColor='green-text' numChange='99' border='tw-border'/>
+                    <BigCard icon={twIcon} user='@mannamar' num='1044' type='FOLLOWERS' arrow={upIcon} textColor='green-text' numChange='99' border='tw-border'/>
                 </Col>
                 <Col xs={12} md={6} lg={3}>
-                    <BigCard icon={igIcon} user='@realnathanf' num='11k' type='FOLLOWERS' arrow={upIcon} textColor='green-text' numChange='1099' border='ig-border'/>
+                    <BigCard icon={igIcon} user='@therealmannamar' num='11k' type='FOLLOWERS' arrow={upIcon} textColor='green-text' numChange='1099' border='ig-border'/>
                 </Col>
                 <Col xs={12} md={6} lg={3}>
-                    <BigCard icon={ytIcon} user='Nathan F.' num='8239' type='SUBSCRIBERS' arrow={downIcon} textColor='red-text' numChange='144' border='yt-border'/>
+                    <BigCard icon={ytIcon} user='Amar M.' num='8239' type='SUBSCRIBERS' arrow={downIcon} textColor='red-text' numChange='144' border='yt-border'/>
                 </Col>
             </Row>
         </Container>
